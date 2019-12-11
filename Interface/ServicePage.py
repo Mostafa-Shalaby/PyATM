9743858d817b7c1c99e92460fee1e04dae90d3ed
Mostafa Y Shalaby <mostafa.shalaby98@gmail.com>
2019-12-11 19:02:46 +0200

    Adds balance and transactions to GUI.
    adds transaction presets for testing and displaying.
    adds limit 10 transaction per query.
    adds currencytype to the transaction result array.
    removes unused result dictionary.

--- Interface/ServicePage.py
+++ Interface/ServicePage.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from pathlib import Path
+from Modules import DbModule
 
 class ServicePage(Frame):
     # Class Description
@@ -11,16 +12,18 @@ class ServicePage(Frame):
         # Creates Few Properties to Manipulate the Interface
         self.Background = parent['bg']
         self.SelectedCard = card
+        self.Parent = parent
 
         # Populates the Frame
-        self.UserDescriptor().grid(row=0,column=0,padx=(0,145),pady=10,sticky=NW)
-        self.ServiceSelector().grid(row=0,column=1,pady=10, sticky=W)
-        self.QuickCash().grid(row=1,column=1,pady=(20,0),sticky=NSEW)
+        self.UserDescriptor().pack(side=LEFT,padx=(0,155),anchor=N)
+        # Create a service variable to deal with it in other classes.
+        self.Selector=self.ServiceSelector()
+        self.Selector.pack(side=LEFT)
 
     def UserDescriptor(self):
         # Method Description:
         """Creates a Frame and populates it with Numbered Buttons"""
-        # Create a Frame to hold the various controls/buttons and sets its Background.
+        # Create a Frame to hold few Labels that provide basic information about the user.
         userFrame = Frame(self, bg=self.Background)
         TitleLabel(userFrame, text="Welcome,").pack(anchor=W)
         DetailsLabel(userFrame, text=self.SelectedCard.UserName).pack(anchor=W)
@@ -30,20 +33,100 @@ class ServicePage(Frame):
 
     def ServiceSelector(self):
         # Method Description:
-        """Creates a Frame and populates it with Numbered Buttons"""
-        # Create a Frame to hold the various controls/buttons and sets its Background.
+        """Creates a Frame and populates it with Services Selection Buttons"""
+        # Create a Frame to a set of buttons that selects various ATM services.
         selectorFrame = Frame(self, bg=self.Background)
-        TitleLabel(selectorFrame, text="Available Services:").grid(row=0,column=0,padx=(5,10),pady=(5,10), sticky=W)
-        ServiceButton(selectorFrame,text="Withdraw").grid(row=1,column=0,padx=(5,10),pady=(5,10))
-        ServiceButton(selectorFrame,text="Deposit").grid(row=1,column=1,padx=(10,5),pady=(5,10))
-        ServiceButton(selectorFrame,text="Balance").grid(row=2,column=0,padx=(5,10),pady=(10,5))
-        ServiceButton(selectorFrame,text="Transactions").grid(row=2,column=1,padx=(10,5),pady=(10,5))
+        TitleLabel(selectorFrame, text="Available Services:").grid(row=0,column=0,padx=(0,10),pady=(5,10),sticky=W)
+        ServiceButton(selectorFrame,text="Withdraw",command=self.WithdrawService).grid(row=1,column=0,padx=(0,10),pady=(5,10))
+        ServiceButton(selectorFrame,text="Deposit",command=self.DepositService).grid(row=1,column=1,padx=(10,0),pady=(5,10))
+        ServiceButton(selectorFrame,text="Balance",command=self.BalanceService).grid(row=2,column=0,padx=(0,10),pady=(10,5))
+        ServiceButton(selectorFrame,text="Transactions",command=self.TransactionsService).grid(row=2,column=1,padx=(10,0),pady=(10,5))
+        QuickButton(selectorFrame,text="Quick Cash - 100 L.E.").grid(row=3,column=0,columnspan=2,pady=(25,0),sticky=EW)
         return selectorFrame
 
-    def QuickCash(self):
-        quickFrame = Frame(self, bg=self.Background)
-        QuickButton(quickFrame,text="Quick Cash - 100 L.E.").pack(expand=1, fill=X)
-        return quickFrame
+    def WithdrawService(self):
+        # Method Description
+        """First Service Avaliable is (Checking Balance)"""
+        # Puts a title for the deposit to be displayed as text by UI elements.
+        withdrawTitle=self.SelectedCard.accountType+" Account's Withdraw:"
+
+        # Hids the Service Options Selector.
+        self.Selector.pack_forget()
+
+        # Create a Frame to hold the balance and populates it with Two Labels.
+        self.CurrentService = withdrawFrame = Frame(self,bg=self.Background)
+        TitleLabel(withdrawFrame,text=withdrawTitle).pack(anchor=W)
+        # Creates a button to go back to service selector.
+        self.SwitchService(withdrawFrame).pack(pady=(25,0))
+        withdrawFrame.pack(side=LEFT)
+    
+    def DepositService(self):
+        # Method Description
+        """First Service Avaliable is (Checking Balance)"""
+        # Puts a title for the deposit to be displayed as text by UI elements.
+        depositTitle=self.SelectedCard.accountType+" Account's Deposit:"
+
+        # Hids the Service Options Selector.
+        self.Selector.pack_forget()
+
+        # Create a Frame to hold the balance and populates it with Two Labels.
+        self.CurrentService = depositFrame = Frame(self,bg=self.Background)
+        TitleLabel(depositFrame,text=depositTitle).pack(anchor=W)
+        # Creates a button to go back to service selector.
+        self.SwitchService(depositFrame).pack(pady=(25,0))
+        depositFrame.pack(side=LEFT)
+
+    def BalanceService(self):
+        # Method Description
+        """First Service Avaliable is (Checking Balance)"""
+        # Gets the current Balance from the Database.
+        currentBalance, currency = DbModule.BalanceCheck(self.SelectedCard.ID, self.SelectedCard.accountType)       
+        # Puts the Balance in a string and a title to be displayed as text by UI elements.
+        balanceTitle=self.SelectedCard.accountType+" Account's Balance:"
+        balanceString=str(currentBalance)+' '+currency
+
+        # Hids the Service Options Selector.
+        self.Selector.pack_forget()
+
+        # Create a Frame to hold the balance and populates it with Two Labels.
+        self.CurrentService = balanceFrame = Frame(self,bg=self.Background)
+        TitleLabel(balanceFrame,text=balanceTitle).pack(anchor=W)
+        Label(balanceFrame,text=balanceString,font=("Calibri",28),bg="#0c3b97",fg="#1a96dc").pack(anchor=W)
+        # Creates a button to go back to service selector.
+        self.SwitchService(balanceFrame).pack(pady=(25,0))
+        balanceFrame.pack(side=LEFT)
+
+    def TransactionsService(self):
+        # Method Description
+        """First Service Avaliable is (Checking Balance)"""
+        # Gets the transactions from the database
+        transactions=DbModule.TransCheck(self.SelectedCard.ID, self.SelectedCard.accountType)
+        transTitle=self.SelectedCard.accountType+" Account's Transactions:"
+        # Hids the Service Options Selector.
+        self.Selector.pack_forget()
+
+        # Create a Frame to hold the balance and populates it with Two Labels.
+        self.CurrentService = transFrame = Frame(self,bg=self.Background)
+        TitleLabel(transFrame,text=transTitle).pack(padx=(0,10),pady=(5,10),anchor=W)
+        for x in transactions:
+            transText="On "+str(x[3])+', '+str(x[0]).capitalize()+' '+str(x[1])+' '+str(x[2])
+            Label(transFrame,text=transText,font=("Calibri",18),bg="#0c3b97",fg="#1a96dc").pack(anchor=W)
+        # Creates a button to go back to service selector.
+        self.SwitchService(transFrame).pack(pady=(25,0))
+        transFrame.pack(side=LEFT)
+    
+    def SwitchService(self,parent):
+        # Method Description:
+        """Creates a Frame and populates it with Services Selection Buttons"""
+        # Create a Frame to a set of buttons that selects various ATM services.
+        switchFrame = Frame(parent, bg=self.Background)
+        SwitchButton(switchFrame,bg="#e5ac00",hbg="#ffbf00",abg="#bf8f00",text="Select Another Service",command=self.BackToSelector).grid(row=0,column=0,padx=(0,10))
+        SwitchButton(switchFrame,bg="#cc0058",hbg="#eb0066",abg="#990042",text="Return Card",command=self.Parent.ReturnCard).grid(row=0,column=1,padx=(10,0))
+        return switchFrame
+
+    def BackToSelector(self):
+        self.CurrentService.pack_forget()
+        self.Selector.pack(side=LEFT)
 
 
 # Hardcoded Stylized Widgets: (Subclass of Existing Widgets)
@@ -61,8 +144,6 @@ class ServiceButton(Button):
         self['fg']="#f1f1f1"
         self['activebackground'] = "#2257bf"
         self['activeforeground'] = "#afafaf"
-        # self.photo = PhotoImage(file=Path(__file__).parent / photoname)  
-        # self.config(image=self.photo, compound = CENTER)
         # Binds the hover over events to a set of functions
         self.bind("<Enter>", self.OnEnter)
         self.bind("<Leave>", self.OnLeave)
@@ -72,6 +153,31 @@ class ServiceButton(Button):
     # When Mouse Leaves
     def OnLeave(self, e):
         self['bg'] = "#0077cc"
+
+class SwitchButton(Button):
+     # Class Description
+    """Stylized Buttons For Use In ATM Service Selection Controls"""
+    def __init__(self, parent, hbg, abg, *args, **kwargs):
+        # Initalizes A Button, and Hard Codes Few Styles.
+        Button.__init__(self, parent, *args, **kwargs)
+        self['width'] = 26
+        self['height'] = 1
+        self['border'] = 0
+        self['font'] = ("Calibri", 19, "bold")
+        self['fg']="#f1f1f1"
+        self['activebackground'] = abg
+        self['activeforeground'] = "#afafaf"
+        self.Background = self['bg']
+        self.HoverColor = hbg
+        # Binds the hover over events to a set of functions
+        self.bind("<Enter>", self.OnEnter)
+        self.bind("<Leave>", self.OnLeave)
+    # When Mouse is Over
+    def OnEnter(self, e):
+        self['bg'] = self.HoverColor
+    # When Mouse Leaves
+    def OnLeave(self, e):
+        self['bg'] = self.Background
 
 class QuickButton(Button):
     # Class Description
@@ -101,8 +207,6 @@ class TitleLabel(Label):
         # Initalizes A Button, and Hard Codes Few Styles.
         Label.__init__(self, parent, *args, **kwargs)
         self['font'] = ("Calibri", 24, "bold")
-        self['border'] = 0
-        self['justify'] = LEFT
         self['bg'] = "#0c3b97"
         self['fg'] = "#f1f1f1"
     
@@ -111,8 +215,5 @@ class DetailsLabel(Label):
         # Initalizes A Button, and Hard Codes Few Styles.
         Label.__init__(self, parent, *args, **kwargs)
         self['font'] = ("Calibri", 18)
-        self['border'] = 0
-        self['anchor'] = W
-        self['justify'] = LEFT
         self['bg'] = "#0c3b97"
         self['fg'] = "#1a96dc"
--- Modules/DbModule.py
+++ Modules/DbModule.py
@@ -1,6 +1,7 @@
 # Imported Modules
 import sqlite3
 import os
+import time
 
 # Name and Path of the Database File, (Since there is no path, it will be in the root of the project!)
 DbFile = "PyATM.db"
@@ -76,11 +77,35 @@ def PopulateDatabase():
     cur.execute("INSERT INTO Account (cardid, username, accounttype, balance, currencytype) VALUES ('372995442619905', 'Ahmed Saeed', 'Checking', 500.00, 'L.E.');")
 
     # Populates the Transactions Table With Few Preset Data.
-    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype) VALUES (1, '372995442619905', 'withdraw', 5000, 'L.E.')")
-    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype) VALUES (1, '372995442619905', 'deposit', 9500, 'L.E.')")
-    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype) VALUES (4, '377557997331297', 'withdraw', 750, 'L.E.')")
-    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype) VALUES (1, '372995442619905', 'withdraw', 2911, 'L.E.')")
-    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype) VALUES (5, '4445572056242236', 'deposit', 7900, 'L.E.')")
+    # Preset Values for Card 1
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (1, '372995442619905', 'deposit', 10000, 'L.E.','2019-12-06 09:08:11')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (1, '372995442619905', 'withdraw', 5000, 'L.E.','2019-12-08 13:30:33')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (1, '372995442619905', 'withdraw', 5000, 'L.E.','2019-12-08 20:40:12')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (1, '372995442619905', 'deposit', 9500, 'L.E.','2019-12-09 03:11:11')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (1, '372995442619905', 'withdraw', 2911, 'L.E.','2019-12-10 16:22:10')")
+    # Preset Values For Card 2
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (2, '5504981108311326', 'deposit', 11000, 'L.E.','2019-12-04 10:15:45')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (2, '5504981108311326', 'deposit', 12000, 'L.E.','2019-12-05 14:54:33')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (2, '5504981108311326', 'withdraw', 1000, 'L.E.','2019-12-06 09:08:11')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (2, '5504981108311326', 'deposit', 20000, 'L.E.','2019-12-07 11:30:33')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (2, '5504981108311326', 'deposit', 18000, 'L.E.','2019-12-08 14:22:47')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (2, '5504981108311326', 'withdraw', 5000, 'L.E.','2019-12-09 13:30:33')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (2, '5504981108311326', 'withdraw', 8000, 'L.E.','2019-12-10 14:33:10')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (2, '5504981108311326', 'deposit', 4000, 'L.E.','2019-12-11 19:22:12')")
+    # Preset Values For Card 3
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (3, '4445544517086312', 'deposit', 6000, 'L.E.','2019-12-04 10:15:45')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (3, '4445544517086312', 'deposit', 2000, 'L.E.','2019-12-05 14:54:33')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (3, '4445544517086312', 'withdraw', 3000, 'L.E.','2019-12-08 14:22:47')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (3, '4445544517086312', 'withdraw', 1000, 'L.E.','2019-12-10 14:33:10')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (3, '4445544517086312', 'deposit', 5000, 'L.E.','2019-12-11 19:22:12')")
+    # Preset Values For Card 4
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (4, '4445544517086312', 'deposit', 700, 'L.E.','2019-12-04 10:15:45')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (4, '4445544517086312', 'deposit', 2800, 'L.E.','2019-12-05 14:54:33')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (4, '4445544517086312', 'withdraw', 1200, 'L.E.','2019-12-08 14:22:47')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (4, '4445544517086312', 'withdraw', 1800, 'L.E.','2019-12-10 14:33:10')")
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (4, '4445544517086312', 'withdraw', 311, 'L.E.','2019-12-11 19:22:12')")
+    # Preset Values For Card 5
+    cur.execute("INSERT INTO Transactions (userid, cardid, type, amount, currencytype, occurrence) VALUES (5, '4445572056242236', 'deposit', 7900, 'L.E.','2019-12-06 03:11:11')")
 
     # Commits/Saves the changes to the database
     dbConnection.commit()
@@ -151,15 +176,9 @@ def TransCheck(cardid, accountType):
     cur.execute("SELECT id, currencytype FROM Account WHERE cardid = ? AND accounttype = ?", (cardid, accountType))
     id, currency = cur.fetchone()
     # Retrieve the transaction history from the database
-    cur.execute("SELECT type, amount, occurrence FROM Transactions t INNER JOIN Account a ON a.id = t.userid WHERE userid = ? ORDER BY occurrence DESC;", (id, ))
+    cur.execute("SELECT t.type, t.amount, t.currencytype, t.occurrence FROM Transactions t INNER JOIN Account a ON a.id = t.userid WHERE userid = ? ORDER BY occurrence DESC LIMIT 10;", (id, ))
     # Store the matching rows in a dictionary
-    matches = cur.fetchall()
-    result = []
-    for match in matches:
-        d = {'occurrence': match[2],
-             'amount': match[1],
-             'type': match[0]}
-        result.append(d)
+    result = cur.fetchall()
     # Commits/Saves the changes to the database
     dbConnection.commit()
     # Closes Both the unused Connection and Cursor
